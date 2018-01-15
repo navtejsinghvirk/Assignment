993beb5b73ab72ef8dfe013548452d40fb2852c9
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-15 16:43:00 -0500

    function sort array

--- arrayfunction.php
+++ arrayfunction.php
@@ -1,1 +1,38 @@
 <!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array Function Assign</title>
+ </head>
+ <body>
+      <?php
+
+        $animals = array("panda","alpaca","boa");
+
+        function sortanimal(){
+            global $animals;
+            for($x=0;$x<=count($animals);$x++){
+                sort($animals);
+                echo $animals[$x];
+                echo "    ";
+            }
+        }
+            //echo "$cup_value";
+            //echo "<br>";
+          // }
+
+             echo"<br>";
+             sortanimal();  //alpaca boa panda
+             echo"<br>";
+            // addanimals(goat); //alpaca boa goat panda
+             echo"<br>";
+             //addanimals(Boa); //alpaca boa goat panda
+              echo"<br>";
+              echo"<br>";
+
+        ?>
+ </body>
+</html>
