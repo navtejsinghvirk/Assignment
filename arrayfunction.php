e9c3d4e8e7aebe4dcc5cfe6412c784cffc7930ce
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-15 19:12:11 -0500

    Create Associate array & display check box

--- arrayfunction.php
+++ arrayfunction.php
@@ -9,8 +9,10 @@
  </head>
  <body>
       <?php
+      //define array as $animals
       $animals = array("panda","alpaca","boa");
-      function sortanimal(){
+      //function for sort animals
+      function sortAnimal(){
             global $animals;
             for($x=0;$x<=count($animals);$x++){
                 sort($animals);
@@ -18,34 +20,60 @@
                 echo "    ";
                }
             }
-
+     // function add new animals in array and check case insensitive
       function addanimals($newanimal){
           global $animals;
-          $newlower =strtolower($newanimal);
+          $newlowerchar = strtolower($newanimal);
           for($x=0;$x<=count($animals);$x++){
-              if(in_array($newlower,$animals))
+              if(in_array($newlowerchar,$animals))
               {
                   sort($animals);
                   echo $animals[$x];
                   echo "    ";
               }else{
-                  array_push($animals,$newlower);
+                  array_push($animals,$newlowerchar);
                   sort($animals);
                   echo $animals[$x];
                   echo "    ";
                }
               }
             }
+     //call functions
+      sortAnimal();  //alpaca boa panda
+       echo"<br>";
+      addanimals(goat); //alpaca boa goat panda
+         echo"<br>";
+      addanimals(Boa); //alpaca boa goat panda
+      echo "<br>";
+      echo"<br>";
 
-             echo"<br>";
-             sortanimal();  //alpaca boa panda
-             echo"<br>";
-             addanimals(goat); //alpaca boa goat panda
-             echo"<br>";
-            addanimals(Boa); //alpaca boa goat panda
-             echo "<br>";
-             // echo"<br>";
+   // define associative array key=>value pair
+      $cupcake = array("grasshopper"=>"The Grasshopper",
+                          "maple"=>"Whisky Maple Bacon",
+                          "carrot"=>"Carrot Walnut",
+                          "caramel"=>"Salted Caramel Cupcake",
+                          "velvet"=>"Red Velvet",
+                          "lemon"=>"Lemon Drop",
+                          "tiramisu"=>"Tiramisu");
 
-        ?>
+    // print as key value as checkbox
+      foreach($cupcake as $cup=>$cup_value) {
+          echo' <input type="checkbox" name="cup[]" value ="">';
+          echo "$cup_value";
+          echo "<br>";
+          }
+          ?>
+<br>
+      <label>
+      <!--create individual check box  -->
+      <input type="checkbox" name="flavors[]" value="grasshopper"> The Grasshopper<br>
+      <input type="checkbox" name="flavors[]" value="maple"> Whisky Maple Bacon<br>
+      <input type="checkbox" name="flavors[]" value="carrot"> Carrot Walnut<br>
+      <input type="checkbox" name="flavors[]" value="caramel"> Salted Carmel Cupcake<br>
+      <input type="checkbox" name="flavors[]" value="velvet"> Red Velvet<br>
+      <input type="checkbox" name="flavors[]" value="lemon">Lemon Drop<br>
+      <input type="checkbox" name="flavors[]" value="tiraminsu">Tiramisu<br>
+      </label>
+
  </body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-//require the autolaod file
-require_once ('vendor/autoload.php');
+            //require the autolaod file
+            require_once ('vendor/autoload.php');
 
-//create an instance of the base class
-$f3 = Base::instance();
+            //create an instance of the base class
+            $f3 = Base::instance();
 
-//define a default route
-$f3->route('GET /',
-    function() {
-        $view = new View;
-        echo $view->rendor
-        ('Assignment/arrayfunction.php');
-    }
-);
+            //Set debug level
+            $f3->set('DEBUG',3);
 
-//run fat free
-$f3->run();
+            //define a default route
+            $f3->route('GET /',
+                function() {
+                    $view = new View;
+                    echo $view->rendor
+                    ('Assignment/arrayfunction.php');
+                }
+            );
+
+            //run fat free
+            $f3->run();
 ?>
